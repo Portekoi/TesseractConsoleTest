dad60b1aa8549ab7f103ab0a2be08cf022673a18
Portekoi <portekoi@gmail.com>
2017-03-26 00:19:28 +0100

    First Commit

---
+++ TesseractConsoleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Tesseract;
+using System.Drawing;
+
+namespace TesseractConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string appDirectory = Environment.CurrentDirectory;
+            string path = appDirectory + @"\images\c.jpg";
+            Bitmap image = new Bitmap(path);
+
+            using (var engine = new TesseractEngine(@"./tessdata", "eng"))
+            {
+                engine.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNPQRSTUVWXYZ0123456789");
+
+                using (var page = engine.Process(image, PageSegMode.SingleChar))
+                {
+                    Console.WriteLine(page.GetText());
+                }
+                              
+            }
+
+            Console.Read();
+        }
+    }
+}
